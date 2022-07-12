06d3a195441f401ccc8e366f860b19e339ec4c47
alexmonteil <alex.monteil@outlook.com>
2022-07-12 13:18:04 -0500

    Added ToCamelCase challenge solution

---
+++ ToCamelCase/ToCamelCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+static string ToCamelCase(string str)
+{
+    HashSet<char> chars = new HashSet<char> {'-', '_'};
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int i = 0; i < str.Length - 1; i++)
+    {
+        if (chars.Contains(str[i]))
+        {
+            if (!chars.Contains(str[i + 1]))
+            {
+                str = str.Replace(str[i + 1], Char.ToUpper(str[i + 1]));
+            }
+        }
+    }
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (!chars.Contains(str[i]))
+        {
+            stringBuilder.Append(str[i]);   
+        }
+    }
+
+    return stringBuilder.ToString();
+}
+
+string test = "The_Stealth_Warrior";
+string test2 = "the-stealth-warrior";
+Console.WriteLine(ToCamelCase(test));
+Console.WriteLine("\n");
+Console.WriteLine(ToCamelCase(test2));
