cebe6041351c0bbaceb132591bc42d49ed4c685c
alexmonteil <alex.monteil@outlook.com>
2022-12-13 15:10:43 -0600

    Added OddTimes challenge

---
+++ OddTimes/OddTimes/OddTimes.cs
@@ -0,0 +1,33 @@
+﻿
+namespace OddTimesKata;
+
+public static class OddTimes
+{
+
+    public static int find_it(int[] seq)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        foreach (int i in seq)
+        {
+            if (dict.ContainsKey(i))
+            {
+                dict[i]++;
+            }
+            else
+            {
+                dict.Add(i, 1);
+            }
+        }
+
+        foreach (KeyValuePair<int, int> kvp in dict)
+        {
+            if (kvp.Value % 2 != 0)
+            {
+                return kvp.Key;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ OddTimes/OddTimes/Program.cs
@@ -0,0 +1,5 @@
+﻿using OddTimesKata;
+int[] seq = {1,1,2,3,2};
+
+Console.WriteLine(OddTimes.find_it(seq));
+
